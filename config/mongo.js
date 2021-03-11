7be58eb09bde2a0374939fd9f68b401cdbe0cae3
Fatoumata Diaby <fatoumata.diaby@outlook.com>
2021-03-11 15:50:45 +0100

    deploy

--- config/mongo.js
+++ config/mongo.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost/online-book-club", {
+mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useCreateIndex: true,
   useUnifiedTopology: true,
